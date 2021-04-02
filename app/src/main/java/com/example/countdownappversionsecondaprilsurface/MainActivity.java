6a9bd7f23616ac379bdad7c451454fb98d1f1c66
Hamid <58402951+KeyboardKitten@users.noreply.github.com>
2021-04-02 16:07:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/countdownappversionsecondaprilsurface/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.countdownappversionsecondaprilsurface;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import cn.iwgang.countdownview.CountdownView;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnDatePicker = findViewById(R.id.buttonDatePicker);
+
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String pickerDateString = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        TextView tvDatePicker = findViewById(R.id.dateContext);
+        CountdownView myCountdownView = findViewById(R.id.Counter);
+
+        try {
+            tvDatePicker.setText(pickerDateString);
+            Date now = new Date();
+
+            long currentDate = now.getTime();
+            long pickerDate = calendar.getTimeInMillis();
+            long countDownToPickerDate = pickerDate - currentDate;
+            myCountdownView.start(countDownToPickerDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
