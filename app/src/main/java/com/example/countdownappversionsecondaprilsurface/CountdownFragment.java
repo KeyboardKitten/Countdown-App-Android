6e32a0a39a47efd03bf2cb8193fc7fd0ad0d6cbd
Hamid <58402951+KeyboardKitten@users.noreply.github.com>
2021-04-03 19:04:54 +0100

    FAB button now functioning, sending to editCountdownFragment

--- app/src/main/java/com/example/countdownappversionsecondaprilsurface/CountdownFragment.java
+++ app/src/main/java/com/example/countdownappversionsecondaprilsurface/CountdownFragment.java
@@ -14,6 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CountdownFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
@@ -21,17 +25,20 @@ public class CountdownFragment extends Fragment implements DatePickerDialog.OnDa
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.countdownfragment_main, container, false);
-    AppCompatButton btnDatePicker = v.findViewById(R.id.buttonDatePicker);
-    // https://developer.android.com/reference/androidx/fragment/app/Fragment#getFragmentManager()
-    // Double check Fragment Manager has been removed
-    getChildFragmentManager();
-    btnDatePicker.setOnClickListener(new View.OnClickListener() {
+
+
+
+    FloatingActionButton fabEditButton = v.findViewById(R.id.fabEditIcon);
+
+    fabEditButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            DialogFragment datePicker = new DatePickerFragment();
-            datePicker.show(getChildFragmentManager(), "date picker");
+            MainActivity mainActivity = (MainActivity) getActivity();
+            mainActivity.changeFragment(new EditCountdownFragment(), "edit countdown");
         }
     });
+
+
 
     return v;
     }
