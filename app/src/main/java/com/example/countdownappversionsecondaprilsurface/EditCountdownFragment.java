a87399c376e7875e5d954e0e9cb4ff55989b5735
Hamid <58402951+KeyboardKitten@users.noreply.github.com>
2021-04-04 20:11:46 +0100

    Implemented SharePreferences and Checkboxes
    Checkboxes still need work to fuction

--- app/src/main/java/com/example/countdownappversionsecondaprilsurface/EditCountdownFragment.java
+++ app/src/main/java/com/example/countdownappversionsecondaprilsurface/EditCountdownFragment.java
@@ -1,7 +1,10 @@
 package com.example.countdownappversionsecondaprilsurface;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,19 +13,30 @@ import android.widget.DatePicker;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 public class EditCountdownFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    SharedPreferences sp;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.countdownedit_main, container, false);
 
+        Context context = getContext();
+        if (context == null) return v;
+
+        sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+
         AppCompatButton btnDatePicker = v.findViewById(R.id.buttonDatePicker);
         AppCompatButton saveButton = v.findViewById(R.id.countdownEditSaveBUtton);
         AppCompatButton holidayAPIButton = v.findViewById(R.id.buttonAPIHoliday);
+        final AppCompatEditText textbox = v.findViewById(R.id.countDownNameEditBox);
+
+        textbox.setText(sp.getString("textboxText", ""));
 
         getChildFragmentManager();
         // https://developer.android.com/reference/androidx/fragment/app/Fragment#getFragmentManager()
@@ -40,6 +54,9 @@ public class EditCountdownFragment extends Fragment implements DatePickerDialog.
             public void onClick(View v) {
                 MainActivity mainActivity = (MainActivity) getActivity();
                 mainActivity.changeFragment(new CountdownFragment(), "Countdown Fragment");
+                String textboxContents = textbox.getText().toString();
+
+                sp.edit().putString("textboxText", textboxContents).apply();
             }
         });
 
