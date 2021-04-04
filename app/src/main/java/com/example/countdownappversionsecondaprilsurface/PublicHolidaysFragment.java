0c77c27a7198bdac176d66e316f2a6d84ccb6b87
Hamid <58402951+KeyboardKitten@users.noreply.github.com>
2021-04-04 20:13:52 +0100

    API implemented, needs work

--- app/src/main/java/com/example/countdownappversionsecondaprilsurface/PublicHolidaysFragment.java
+++ app/src/main/java/com/example/countdownappversionsecondaprilsurface/PublicHolidaysFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,65 +18,130 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class PublicHolidaysFragment extends Fragment {
 
     private RequestQueue requestQueue;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.holidayapi_main, container, false);
 
+        Context context = getContext();
+        if (context == null) return v;
+
+        listView = v.findViewById(R.id.holidayAPIListView);
+        arrayAdapter = new ArrayAdapter<String>(
+                context,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>()
+        );
+        listView.setAdapter(arrayAdapter);
+
         requestQueue = Volley.newRequestQueue(getContext());
 
+
+        // Public Gov.UK api for displaying bank holidays
+        // DOCUMENTATION https://www.api.gov.uk/gds/bank-holidays/#bank-holidays
         String url = "https://www.gov.uk/bank-holidays.json";
 
         // THIS WORKS
 
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.GET, // Type of request, using GET to get info
-                url, // url from the gov.uk page
-                new Response.Listener<String>() { // When no error this method runs
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("GOVUK", response);
-                    }
-                },
-                new Response.ErrorListener() { // When there is an error
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("ERRORGOVUK", error.getMessage());
-                    }
-                }
 
 
-        );
-
-        requestQueue.add(stringRequest);
-
-//        int numberOfItem = 10;
-//        ArrayList <String> data = new ArrayList<String>();
-//        for (int i=0; i<numberOfItem; i++)
-//            data.add(String.format("List item: %s", i));
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
 //
-//        Context context = getContext();
-//        if (context == null)
-//            return v;
+//                Request.Method.GET, // Type of request, using GET to get info
+//                url,
+//                null,// url from the gov.uk page
+//                new Response.Listener<JSONObject>() { // When no error this method runs
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        String holidaysPrinted;
 //
-//        ArrayAdapter <String> myArrayAdapter = new ArrayAdapter<String>(
-//                context,
-//                android.R.layout.simple_list_item_1,
-//                data
+//                        try {
+//                            holidaysPrinted = response.getJSONObject("england-and-wales").getJSONObject("division").getJSONObject("events").getString("title");
+//                            Log.d("HOLIDAYS", response.toString(2));
+//
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                            holidaysPrinted = "Unknown";
+//                        }
+//                        listView.(holidaysPrinted);
+//                    }
+//
+//                },
+//
+//
+//
+//                new Response.ErrorListener() { // When there is an error
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        if (error.getMessage() != null) {
+//                            Log.d("ERRORGOVUK", error.getMessage());
+//                        }
+//                    }
+//                }
 //        );
 //
-//        ListView listView = v.findViewById(R.id.holidayAPIListView);
-//        listView.setAdapter(myArrayAdapter);
+//        requestQueue.add(jsonObjectRequest);
+
+        fetchDataAndUpdateList(
+                url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TEST","onResponse Started");
+                        try {
+                            JSONArray events = response.getJSONArray("events");
+                            System.out.println(events);
+                            int l = events.length();
+                            for (int i=0; i < l; i++) {
+                                JSONObject obj = events.getJSONObject(i);
+                                arrayAdapter.add(obj.getString("title"));
+                            }
+                        } catch (JSONException e) {
+                            Log.d("ERROR","JSON Error");
+                        }
+                    }
+                });
 
         return v;
     }
+
+    private void fetchDataAndUpdateList(String url, Response.Listener<JSONObject> onResponse){
+        JsonObjectRequest exampleRequest = new JsonObjectRequest(Request.Method.GET, url, null, onResponse,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR HOLIDAYS", error.getMessage());
+                    }
+                });
+        requestQueue.add(exampleRequest);
+    }
+
+    @Override
+    public void onResume() {
+        requestQueue.start();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        requestQueue.stop();
+        super.onPause();
+    }
+
 }
